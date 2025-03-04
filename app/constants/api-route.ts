e5a400d5ae554754ee1ca18a16bde3840312e7a1
Hannah Catherine Trask <hctrask@gmail.com>
2025-03-03 20:15:25 -0700

    Adds formatted sunrise, creates utils & constants directory

---
+++ app/constants/api-route.ts
@@ -0,0 +1,3 @@
+import {JACKSON_LAT, JACKSON_LON} from "@/app/constants/jackson";
+
+export const SUNRISE_API_ROUTE = 'https://api.sunrise-sunset.org/json?lat=' + JACKSON_LAT + '&lng=' + JACKSON_LON + '&formatted=0'
---
+++ app/constants/jackson.ts
@@ -0,0 +1,2 @@
+export const JACKSON_LAT = '43.48'
+export const JACKSON_LON = '-110.76178'
